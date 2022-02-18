d0a14a578f0b7f35399b4f7a85247221c4249fc5
ArunKurian555 <arunkurian4455@gmail.com>
2022-02-18 14:34:25 -0500

    2022 initial release

---
+++ CBCH5/src/app/view7/view7.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ChRouteService } from 'src/service/ch-route.service';
+import { CdkDragEnd } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
+import { FpzonecontrolComponent } from '../fpzonecontrol/fpzonecontrol.component';
+
+declare var CrComLib: any;
+
+@Component({
+  selector: 'app-view7',
+  templateUrl: './view7.component.html',
+  styleUrls: ['./view7.component.scss']
+})
+export class View7Component implements OnInit {
+  dragPosition = { x: 0, y: 0 };
+  positionarray = [];
+  constructor(public rout: ChRouteService, public dialog: MatDialog) { }
+  items = [1, 2, 3, 4, 5, 6];
+
+  ngOnInit(): void {
+  }
+
+
+
+  clicked()
+
+  {
+    this.rout.active = 3;
+  }
+}
